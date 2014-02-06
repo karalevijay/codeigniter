206fc275ff058c9046c4ef4e76819c09741b079e
karalevijay <karalevijay87@gmail.com>
2014-02-06 15:09:20 +0530

    commited

---
+++ application/config/form_validation.php
@@ -0,0 +1,23 @@
+<?php 
+$config = array(
+array(
+'field' => 'username',
+'label' => 'Username',
+'rules' => 'required'
+),
+array(
+'field' => 'password',
+'label' => 'Password',
+'rules' => 'required'
+),
+array(
+'field' => 'passconf',
+'label' => 'Password Confirmation',
+'rules' => 'required'
+),
+array(
+'field' => 'email',
+'label' => 'Email',
+'rules' => 'required'
+)
+);
