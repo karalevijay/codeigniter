92d6b51877558ae80fb9b7d3347b442adaed3141
karalevijay <karalevijay87@gmail.com>
2014-02-06 16:56:37 +0530

    commited updates

--- application/views/formsuccess.php
+++ application/views/formsuccess.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>My Form</title>
-    </head>sfdss
+    </head>
     <body>
         <h3>Your form was successfully submitted!</h3>
         <p><?php echo anchor('form', 'Try it again!'); ?></p>
